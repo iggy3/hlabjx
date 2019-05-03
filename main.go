c57ad338cff53d7adcf4ecdcf8500ca0765435f2
Iggy <github@poorme.net>
2019-05-02 21:34:04 -0500

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Iggy: Some Jx golang www stuff a"
+	title := "Iggy: Some Jx golang www stuff b"
 
 	from := ""
 	if r.URL != nil {
